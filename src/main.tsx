0fb60d047bb8c01ecd0c902d09823ae5ba023e38
Олег Дидечкин <crm.29431@dz.com>
2023-10-19 22:25:30 +0300

    Added theme and birthdate provider

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from '@/App';
 
+import Providers from '@/components/providers';
+
 import './globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <App />
+    </Providers>
   </React.StrictMode>
 );
